6674a1c1ade319db58f1638c01025f338c834083
planbg <planbgamesec@gmail.com>
2016-10-06 22:24:55 -0500

    Proyecto de android
    Tarea, Mi primera App

---
+++ app/src/main/java/com/planbg/curso2_miprimeraapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.planbg.curso2_miprimeraapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
